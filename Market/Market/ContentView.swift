60605941b985cf4043e0e6c9f48eb5b00f85e567
Roger Trejo <rogtrejo@gmail.com>
2020-01-14 19:37:19 -0800

    initial commit

---
+++ Market/Market/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Market
+//
+//  Created by Roger Trejo on 1/14/20.
+//  Copyright © 2020 Roger Trejo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
